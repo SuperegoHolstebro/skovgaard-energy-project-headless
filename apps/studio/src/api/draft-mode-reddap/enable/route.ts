029dfb02cbd97387a7e96a0a24812e7eef2ce2f7
Kasper Buchholtz <117350672+Kasper-Buchholtz@users.noreply.github.com>
2025-06-01 21:15:47 +0200

    Add draft mode enable and disable route handlers

--- apps/studio/src/api/draft-mode-reddap/enable/route.ts
+++ apps/studio/src/api/draft-mode-reddap/enable/route.ts
@@ -1,7 +1,7 @@
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import { client } from '../../../lib/sanity.client'
 import { readToken } from '../../../lib/sanity.api'
-const token = readToken
+const token = readToken.reddap
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({ token }),
 })
