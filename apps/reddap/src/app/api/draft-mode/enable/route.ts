4b1611798011a9ce5332c3582f50111da7cc3a57
Kasper Buchholtz <117350672+Kasper-Buchholtz@users.noreply.github.com>
2025-06-02 08:37:44 +0200

    Refactor token import in route.ts and add sanity.api.ts for token management

--- apps/reddap/src/app/api/draft-mode/enable/route.ts
+++ apps/reddap/src/app/api/draft-mode/enable/route.ts
@@ -1,7 +1,7 @@
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import { client } from '@repo/utils/lib/sanity.client'
-import { readToken } from '@repo/utils/lib/sanity.api'
-const token = readToken.reddap
+import { readToken } from '../../../../sanity/sanity.api'
+const token = readToken
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({ token }),
 })
