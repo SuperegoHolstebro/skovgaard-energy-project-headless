f57fd81a07277db27f8e1438df958485071ac685
Kasper Buchholtz <117350672+Kasper-Buchholtz@users.noreply.github.com>
2025-06-02 09:27:55 +0200

    Remove legacy tell

--- apps/nordvestjylland/src/app/api/draft-mode/enable/route.ts
+++ apps/nordvestjylland/src/app/api/draft-mode/enable/route.ts
@@ -1,7 +1,7 @@
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import { client } from '@repo/utils/lib/sanity.client'
 import { readToken } from '@repo/utils/lib/sanity.api'
-const token = readToken.nordvestjylland
+const token = readToken
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({ token }),
 })
